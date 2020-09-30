84aeb3dc162c23176217cfb2b1de1d67bcdc0ee7
wangshuyu <wangshuyu@haodf.com>
2020-09-30 10:08:48 +0800

    funcstack init

--- leetcode/20.js
+++ leetcode/20.js
@@ -3,11 +3,16 @@
  * 遇到左括号，就把它放入栈中
  * 遇到右括号，判断和栈顶元素类型是否匹配，不匹配直接返回false,匹配则把栈顶元素弹出
  * 如果最后栈空了，则合法，否则不合法
+ * 时间复杂度O(n)空间复杂度O(n)
+ * @param {string} s
+ * @return {boolean}
+ */
+/**
  * @param {string} s
  * @return {boolean}
  */
 var isValid = function(s) {
-    let result = true;
+    if(s.length % 2 ===1) return false;
     let stack = [];
     for(let i=0;i<s.length;i++){
         const c = s[i];
@@ -26,8 +31,5 @@ var isValid = function(s) {
             }
         }
     }
-    if(stack.length!==0){
-        result = false;
-    }
-    return result;
+    return stack.length === 0;
 };
